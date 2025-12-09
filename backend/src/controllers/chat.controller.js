431cdbe2c6e40a62bd79390eac12c16ec2b73682
Shivam Sharma <meshivam81@gmail.com>
2025-12-09 19:28:12 +0530

    feat(users): generate stream token for chats and video calls (#8)

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,1 +1,15 @@
-export async function getStreamToken(req, res) {}
+import { chatClient } from "../lib/stream.js";
+
+export const getStreamToken = async (req, res) => {
+  try {
+    const token = chatClient.createToken(req.user?.clerkId);
+    res.status(200).json({
+      token,
+      userId: req.user?.clerkId,
+      userName: req.user?.name,
+      userImg: req.user?.image,
+    });
+  } catch (error) {
+    res.status(500).json({ mesaage: "Internal Server Error" });
+  }
+}
--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getStreamToken } from "../controllers/chat.controller.js";
 
 const ChatRoutes = express.Router();
 
-ChatRoutes.get();
+ChatRoutes.get("/token", protectRoute, getStreamToken);
 
 export default ChatRoutes;
