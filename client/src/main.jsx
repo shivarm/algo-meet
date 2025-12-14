f94c66e6719bd72942d69c6aca7184dd0f4c03ea
Shivam Sharma <meshivam81@gmail.com>
2025-12-14 14:57:18 +0530

    feat(client): set-up tailwindcss and daisyui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
