76e9ecd38e6951c5892459a712ecbc75901ad437
Shivam Sharma <meshivam81@gmail.com>
2025-12-27 11:45:46 +0530

    feat: create responsive `Navbar` design (#18)
    * set-up axios and @tanstack/react-query
    
    * feat: implement left nav element
    
    * feat: implement clerk auth button

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router";
+import { ArrowRightIcon, SparklesIcon } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-linear-to-br from-base-100 via-base-200 to-base-300">
+      {/* Navbar */}
+      <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+        <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
+          {/* LOGO */}
+          <Link
+            to={"/"}
+            className="flex items-center gap-3 hover:scale-105 transition-transform duration-200"
+          >
+            <div className="size-10 rounded-xl bg-linear-to-br from-primary via-secondary to-accent flex items-center justify-center shadow-lg">
+              <SparklesIcon className="size-6 text-white" />
+            </div>
+
+            <div className="flex flex-col">
+              <span className="font-black text-xl bg-linear-to-br from-primary via-secondary to-accent bg-clip-text text-transparent font-mono tracking-wider">
+                Talent IQ
+              </span>
+              <span className="text-xs text-base-content/60 font-medium -mt-1">
+                pair programming
+              </span>
+            </div>
+          </Link>
+          {/* Auth btn */}
+          <SignInButton mode="modal">
+            <button className="group px-6 py-3 bg-linear-to-r from-primary to-secondary
+             text-white rounded-xl font-semibold text-sm hover:shadow-xl transition-all 
+             duration-200 hover:scale-105 flex items-center gap-2">
+              <span>Getting Started</span>
+              <ArrowRightIcon className="size-4"/>
+             </button>
+          </SignInButton>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default HomePage;
