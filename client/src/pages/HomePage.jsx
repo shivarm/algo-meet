ba783b1ce579c0b4ac0c7551061e01d7f0d69ab2
Shivam Sharma <meshivam81@gmail.com>
2025-12-28 13:15:31 +0530

    feat(ui): implement hero section elements

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
-import { ArrowRightIcon, SparklesIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon, SparklesIcon, VideoIcon, ZapIcon } from "lucide-react";
 import { SignInButton } from "@clerk/clerk-react";
 
 const HomePage = () => {
@@ -29,15 +29,103 @@ const HomePage = () => {
           </Link>
           {/* Auth btn */}
           <SignInButton mode="modal">
-            <button className="group px-6 py-3 bg-linear-to-r from-primary to-secondary
+            <button
+              className="group px-6 py-3 bg-linear-to-r from-primary to-secondary
              text-white rounded-xl font-semibold text-sm hover:shadow-xl transition-all 
-             duration-200 hover:scale-105 flex items-center gap-2">
+             duration-200 hover:scale-105 flex items-center gap-2"
+            >
               <span>Getting Started</span>
-              <ArrowRightIcon className="size-4"/>
-             </button>
+              <ArrowRightIcon className="size-4" />
+            </button>
           </SignInButton>
         </div>
       </nav>
+
+      {/* Hero Section */}
+      <div className="max-w-7xl mx-auto px-4 py-20">
+        <div className="grid lg:grid-cols-2 gap-12 items-center">
+          {/* Left design */}
+          <div className="space-y-8">
+            <div className="badge badge-primary badge-lg">
+              <ZapIcon className="size-5" />
+              Real Time programming with collaborator
+            </div>
+            <h1 className="text-5xl lg:text-7xl font-black leading-tight">
+              <span
+                className="bg-linear-to-r from-primary via-secondary to-accent
+               bg-clip-text text-transparent"
+              >
+                Code Together,
+              </span>
+              <br />
+              <span className="text-base-content">Learn Together</span>
+            </h1>
+            <p className="mt-4 text-lg text-base-content/80 max-w-xl">
+              Unlock your coding potential with real-time pair programming, instant feedback, and
+              seamless collaboration. Whether you're preparing for interviews or building projects,
+              Talent IQ empowers you to grow, connect, and succeed—together.
+            </p>
+
+            <div className="flex flex-wrap gap-3">
+              <div className="badge badge-lg badge-outline">
+                <CheckIcon className="size-4 text-success" />
+                Live Video Chats
+              </div>
+              <div className="badge badge-lg badge-outline">
+                <CheckIcon className="size-4 text-success" />
+                Code Editor
+              </div>
+              <div className="badge badge-lg badge-outline">
+                <CheckIcon className="size-4 text-success" />
+                Multi-Language
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              <SignInButton mode="modal">
+                <button className="btn btn-primary btn-lg">
+                  Start Coding now
+                  <ArrowRightIcon className="size-4" />
+                </button>
+              </SignInButton>
+              <button className="btn btn-outline btn-lg">
+                <VideoIcon className="size-5" /> Watch Demo
+              </button>
+            </div>
+            {/* Stats */}
+            <div className="flex flex-wrap gap-4">
+              <div className="flex flex-col items-center">
+                <span className="text-4xl lg:text-5xl font-extrabold bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent">
+                  10K+
+                </span>
+                <span className="mt-2 text-base font-medium text-base-content/70">
+                  Active Users
+                </span>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <span className="text-4xl lg:text-5xl font-extrabold bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent">
+                  50K+
+                </span>
+                <span className="mt-2 text-base font-medium text-base-content/70">Sessions</span>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <span className="text-4xl lg:text-5xl font-extrabold bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent">
+                  99.9%
+                </span>
+                <span className="mt-2 text-base font-medium text-base-content/70">Uptime</span>
+              </div>
+            </div>
+          </div>
+          {/* Right */}
+          <img
+            src="/hero.png"
+            alt=""
+            className="w-full h-auto rounded-3xl shadow-2xl border-4 border-base-100 
+           hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+      </div>
     </div>
   );
 };
