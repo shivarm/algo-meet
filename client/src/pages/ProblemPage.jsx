550cd34375455e0c1b088a10cf9caedc63a6a6c3
Shivam Sharma <meshivam81@gmail.com>
2026-01-01 17:46:39 +0530

    feat(ui): create problem page header and elements (#23)

--- client/src/pages/ProblemPage.jsx
+++ client/src/pages/ProblemPage.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router";
+import { PROBLEMS } from "../data/problems";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
+import { getDifficultyBadgeClass } from "../lib/utils";
 
 const ProblemPage = () => {
+  const problems = Object.values(PROBLEMS);
+
   return (
     <div className="min-h-screen bg-base-200">
       <Navbar />
+      <div className="max-w-6xl mx-auto px-4 py-12">
+        {/* Header */}
+        <div className="mb-8">
+          <h1 className="text-3xl md:text-4xl font-bold mb-2">Practice Problems</h1>
+          <p className="text-base-content/70">
+            Sharpen your skills with curated coding challenges!
+          </p>
+        </div>
+        {/* Problems */}
+        <div className="space-y-4">
+          {problems.map((problem) => (
+            <Link
+              key={problem.id}
+              to={`/problem/${problem.id}`}
+              className="card bg-base-100 hover:scale-[1.01] transition-transform"
+            >
+              <div className="card-body">
+                <div className="flex items-center justify-between gap-4">
+                  {/* Left element */}
+                  <div className="flex-1">
+                    <div className="flex items-center gap-3 mb-2">
+                      <div className="size-12 rounded-lg bg-primary/10 flex items-center justify-center">
+                        <Code2Icon className="size-6 text-primary" />
+                      </div>
+                      <div className="flex-1">
+                        <div className="flex items-center gap-2 mb-1">
+                          <h2 className="text-xl font-bold">{problem.title}</h2>
+                          <span className={`badge ${getDifficultyBadgeClass(problem.difficulty)}`}>
+                            {problem.difficulty}
+                          </span>
+                        </div>
+                        <p className="text-sm text-base-content/60"> {problem.category}</p>
+                      </div>
+                    </div>
+                    <p className="text-base-content/80 mb-3">{problem.description.text}</p>
+                  </div>
+                  {/* Right elemnt */}
+                  <div className="flex items-center gap-2 text-primary">
+                    <span className="font-medium">Solve</span>
+                    <ChevronRightIcon className="size-5" />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
