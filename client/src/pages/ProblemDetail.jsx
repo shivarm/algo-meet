13cdb0fa6b2dd5b424ea7180415797632858e3be
Shivam Sharma <meshivam81@gmail.com>
2026-02-15 12:13:31 +0530

    feat(ui): add problem details, code editor and output pannel

--- client/src/pages/ProblemDetail.jsx
+++ client/src/pages/ProblemDetail.jsx
@@ -1,44 +1,73 @@
-import { useEffect, useState } from 'react';
-import {useNavigate, useParams } from 'react-router'
-import { PROBLEMS } from '../data/problems';
-import Navbar from '../components/Navbar';
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { PROBLEMS } from "../data/problems";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Navbar from "../components/Navbar";
+import ProblemDescription from "../components/ProblemDescription";
+import CodeEditor from "../components/CodeEditor";
+import OutputPanel from "../components/OutputPanel";
 
 const ProblemDetail = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const [currentProblemId, setCurrentProblemId] = useState("two-sum");
-    const [selectedLanguage, setSelectedLanguage] = useState("javascript");
-    const [code, setCode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
-    const [isRunning, setIsRunning] = useState(false);
-    const [output, setOutPut] = useState(false);
+  const [currentProblemId, setCurrentProblemId] = useState("two-sum");
+  const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+  const [code, setCode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
+  const [isRunning, setIsRunning] = useState(false);
+  const [output, setOutPut] = useState(false);
 
-    const currentProblem = PROBLEMS[currentProblemId];
+  const currentProblem = PROBLEMS[currentProblemId];
 
-    // Update problem when URL param change
-    useEffect(() => {
-      if (id && PROBLEMS[id]) {
-        setCurrentProblemId(id);
-        setCode(PROBLEMS[id].starterCode[selectedLanguage]);
-        setOutPut(null);
-      }
-    }, [id, selectedLanguage]);
+  // Update problem when URL param change
+  useEffect(() => {
+    if (id && PROBLEMS[id]) {
+      setCurrentProblemId(id);
+      setCode(PROBLEMS[id].starterCode[selectedLanguage]);
+      setOutPut(null);
+    }
+  }, [id, selectedLanguage]);
 
-    const handleLanguageChange = () => {};
+  const handleLanguageChange = () => {};
 
-    const handleProblemChange = () => {};
+  const handleProblemChange = () => {};
 
-    const triggerConfetti = () => {};
+  const triggerConfetti = () => {};
 
-    const checkIfTestsPassed = () => {};
+  const checkIfTestsPassed = () => {};
 
-    const handleRunCode = () => {};
+  const handleRunCode = () => {};
 
-   return (
-    <div className='border border-red-500 h-screen w-screen bg-base-100 flex flex-col'>
-    <Navbar />
+  return (
+    <div className="border border-red-500 h-screen w-screen bg-base-100 flex flex-col">
+      <Navbar />
+
+      <div className="flex-1">
+        <PanelGroup direction="horizontal">
+          <Panel defaultSize={40} minSize={30}>
+            {/* left pannel  */}
+            <ProblemDescription />
+          </Panel>
+
+          <PanelResizeHandle className="w-2 bg-base-300 hover:bg-primary transition-colors cursor-col-resize" />
+
+          <Panel defaultSize={40} minSize={30}>
+            {/* right  pannel  */}
+            <PanelGroup direction="vertical">
+              {/*top pannel- Code editor */}
+              <Panel defaultSize={70} minSize={30}>
+                <CodeEditor />
+              </Panel>
+              <PanelResizeHandle className="h-2 bg-base-300 hover:bg-primary transition-colors cursor-col-resize" />
+              <Panel defaultSize={30} minSize={30}>
+                <OutputPanel />
+              </Panel>
+            </PanelGroup>
+          </Panel>
+        </PanelGroup>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProblemDetail;
